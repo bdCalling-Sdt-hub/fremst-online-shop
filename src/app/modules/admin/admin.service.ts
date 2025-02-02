51cb01f6c297aaf28b564d8f4835f0aef4c2a5c5
Asaduzzama-n <asad_uzzaman@outlook.com>
2025-02-02 15:20:17 +0600

    updated

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -20,7 +20,7 @@ import { handleObjectUpdate } from '../user/user.utils'
 
 //company profile information 
 const getCompanyProfileInformation = async (company_id: string) => {
-  const company = await Company.findById(company_id,{user:1, totalEmployees:1, totalOrders:1, totalBudget:1, totalSpentBudget:1}).populate({
+  const company = await Company.findById(company_id, { user: 1, totalEmployees: 1, totalOrders: 1, totalBudget: 1, totalSpentBudget: 1 }).populate({
     path: 'user',
     select: {
       name: 1,
@@ -28,9 +28,10 @@ const getCompanyProfileInformation = async (company_id: string) => {
       address: 1,
       profile: 1,
       contact: 1,
-      status: 1,},
+      status: 1,
+    },
   })
-  
+
 
 
   if (!company) {
@@ -42,25 +43,25 @@ const getCompanyProfileInformation = async (company_id: string) => {
 
 
 const getEmployeeProfileInformationFromDB = async (employee_id: string) => {
-  const employee = await Employee.findById(employee_id,{createdAt:0, updatedAt:0}).populate({
+  const employee = await Employee.findById(employee_id, { createdAt: 0, updatedAt: 0 }).populate({
     path: 'user',
     select: {
       name: 1,
       email: 1,
       address: 1,
       contact: 1,
-      profile:1,
+      profile: 1,
       status: 1,
     },
   }).populate({
     path: 'company',
     select: {
-      user:1,
-      _id:1,
+      user: 1,
+      _id: 1,
     },
-    populate:{
-      path:'user',
-      select:'name email address contact status profile'
+    populate: {
+      path: 'user',
+      select: 'name email address contact status profile'
     }
 
   })
@@ -68,7 +69,7 @@ const getEmployeeProfileInformationFromDB = async (employee_id: string) => {
   if (!employee) {
     throw new ApiError(StatusCodes.NOT_FOUND, 'Employee not found')
   }
-  
+
   return employee
 }
 
@@ -79,89 +80,89 @@ const getCompaniesFromDB = async (
   paginationOptions: IPaginationOptions,
 
 ): Promise<IGenericResponse<ICompany[]>> => {
-  
-  
-const { page, limit, skip, sortBy, sortOrder } =
-  paginationHelper.calculatePagination(paginationOptions)
-
-const { searchTerm,...filterData } = filters
-
-const andConditions = []
-
-if (searchTerm) {
-  andConditions.push({
-    $or: [
-      { 'user.name': { $regex: searchTerm, $options: 'i' } },
-      { 'user.email': { $regex: searchTerm, $options: 'i' } },
-      { 'user.address': { $regex: searchTerm, $options: 'i' } },
-    ],
-  })
-}
-
-if (Object.keys(filterData).length) {
-  andConditions.push({
-    $and: Object.entries(filterData).map(([field, value]) => ({
-      [field]: value,
-    })),
-  })
-}
 
 
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelper.calculatePagination(paginationOptions)
 
-const whereConditions = andConditions.length > 0 ? { $and: andConditions } : {}
+  const { searchTerm, ...filterData } = filters
 
+  const andConditions = []
 
-
-const result = await Company.aggregate([
-  {
-    $lookup: {
-      from: 'users',
-      localField: 'user',
-      foreignField: '_id',
-      as: 'user',
-    },
-  },
-  { $unwind: '$user' },
-  { $match: whereConditions },
-  {
-    $facet: {
-      total: [{ $count: 'count' }],
-      data: [
-        {
-          $project: {
-            _id: 1,
-            totalEmployees: 1,
-            totalOrders: 1,
-            totalBudget: 1,
-            totalSpentBudget: 1,
-            'user.name': 1,
-            'user.email': 1,
-            'user.address': 1,
-            'user.contact': 1,
-            'user.status': 1,
-            'user.profile': 1,
-          },
-        },
-        { $sort: { [sortBy]: sortOrder === 'asc' ? 1 : -1 } },
-        { $skip: skip },
-        { $limit: limit },
+  if (searchTerm) {
+    andConditions.push({
+      $or: [
+        { 'user.name': { $regex: searchTerm, $options: 'i' } },
+        { 'user.email': { $regex: searchTerm, $options: 'i' } },
+        { 'user.address': { $regex: searchTerm, $options: 'i' } },
       ],
+    })
+  }
+
+  if (Object.keys(filterData).length) {
+    andConditions.push({
+      $and: Object.entries(filterData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    })
+  }
+
+
+
+  const whereConditions = andConditions.length > 0 ? { $and: andConditions } : {}
+
+
+
+  const result = await Company.aggregate([
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'user',
+        foreignField: '_id',
+        as: 'user',
+      },
     },
-  },
-])
+    { $unwind: '$user' },
+    { $match: whereConditions },
+    {
+      $facet: {
+        total: [{ $count: 'count' }],
+        data: [
+          {
+            $project: {
+              _id: 1,
+              totalEmployees: 1,
+              totalOrders: 1,
+              totalBudget: 1,
+              totalSpentBudget: 1,
+              'user.name': 1,
+              'user.email': 1,
+              'user.address': 1,
+              'user.contact': 1,
+              'user.status': 1,
+              'user.profile': 1,
+            },
+          },
+          { $sort: { [sortBy]: sortOrder === 'asc' ? 1 : -1 } },
+          { $skip: skip },
+          { $limit: limit },
+        ],
+      },
+    },
+  ])
 
-const total = result[0].total[0]?.count || 0
-const data = result[0].data
+  const total = result[0].total[0]?.count || 0
+  const data = result[0].data
 
-return {
-  meta: {
-    page,
-    limit,
-    total,
-    totalPage: Math.ceil(total / limit),
-  },
-  data,
-}
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+      totalPage: Math.ceil(total / limit),
+    },
+    data,
+  }
 
 
 }
@@ -169,14 +170,14 @@ return {
 const getEmployeesFromDB = async (
   filters: IEmployeeFilters,
   paginationOptions: IPaginationOptions,
-  user:JwtPayload
+  user: JwtPayload
 ): Promise<IGenericResponse<IEmployee[]>> => {
   const { page, limit, skip, sortBy, sortOrder } =
     paginationHelper.calculatePagination(paginationOptions)
 
   const { searchTerm, companyId } = filters
   companyId ? filters.companyId = companyId : filters.companyId = user.userId
-  console.log(companyId,"companyId")
+  console.log(companyId, "companyId")
   const result = await Employee.aggregate([
     {
       $lookup: {
@@ -249,117 +250,113 @@ const getEmployeesFromDB = async (
   }
 }
 
-const updateEmployee = async (id: Types.ObjectId, payload:Partial<IEmployee & IUser & {budgetUpdate?:boolean}>) => {
-const session = await mongoose.startSession();
-session.startTransaction();
+const updateEmployee = async (id: Types.ObjectId, payload: Partial<IEmployee & IUser & { budgetUpdate?: boolean }>) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-try {
-  const {name, email, address, contact, profile, ...restData} = payload;
+  try {
+    const { name, email, address, contact, profile, ...restData } = payload;
 
-  const isUserExist = await Employee.findOne({user: id}).populate('user',{name: 1, email: 1, address: 1, contact: 1, status: 1, role: 1}).session(session);
-  if (!isUserExist) {
-    throw new ApiError(StatusCodes.NOT_FOUND, 'Requested user does not exist');
+    const isUserExist = await Employee.findOne({ user: id }).populate('user', { name: 1, email: 1, address: 1, contact: 1, status: 1, role: 1 }).session(session);
+    if (!isUserExist) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Requested user does not exist');
+    }
+
+    const updateFields: Partial<IEmployee & IUser & { budgetUpdate?: boolean }> = {};
+
+    let userCollectionUpdatedFields: Partial<IUser> = { name, email, address, contact, profile };
+
+
+    if (address && Object.keys(address).length > 0) {
+      userCollectionUpdatedFields.address = address;
+    }
+
+
+    if (Object.keys(userCollectionUpdatedFields).length > 0) {
+      const updatedUser = await User.findByIdAndUpdate(id, { $set: userCollectionUpdatedFields }, { new: true, session });
+
+      if (!updatedUser) {
+        throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to update user data');
+      }
+    }
+
+    const { budget, duration, budgetUpdate } = restData;
+
+    if (Boolean(budgetUpdate)) {
+      if (!budget || !duration) {
+        throw new ApiError(StatusCodes.BAD_REQUEST, 'Budget and duration are required');
+      }
+
+      const company = await Company.findById(isUserExist.company).session(session);
+      if (!company) {
+        throw new ApiError(StatusCodes.NOT_FOUND, 'Requested company does not exist');
+      }
+
+      const totalBudget = company.totalBudget + budget!;
+      company.totalBudget = totalBudget;
+
+
+      const updatedCompany = await company.save({ session });
+
+      if (!updatedCompany) {
+        throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to update company data');
+      }
+
+      const startDate = new Date()
+
+      const endDate = new Date(startDate)
+      endDate.setMonth(startDate.getMonth() + duration!)
+
+      const endDateISO = endDate.toISOString()
+
+      restData.budgetAssignedAt = startDate;
+      restData.budgetExpiredAt = endDateISO as unknown as Date
+      restData.budget = budget;
+      restData.totalBudget = budget! + isUserExist.budget || 0;
+      restData.budgetLeft = budget! + isUserExist.budgetLeft || 0;
+      restData.duration = duration!;
+
+
+    }
+
+    const updatedEmployee = await Employee.findOneAndUpdate({ user: id }, { $set: restData }, {
+      new: true,
+      session
+    });
+
+    if (!updatedEmployee) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to update employee data');
+    }
+
+    await session.commitTransaction();
+    return updatedEmployee;
+  } catch (error) {
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    session.endSession();
   }
-
-  const updateFields: Partial<IEmployee & IUser & {budgetUpdate?:boolean}> = {};
-
-  let userCollectionUpdatedFields: Partial<IUser> = {name, email, address, contact, profile};
-
-
-  if (address && Object.keys(address).length > 0) {
-     userCollectionUpdatedFields = handleObjectUpdate(
-      address,
-      userCollectionUpdatedFields,
-      'address'
-    )
-    }
-
-  
-  if (Object.keys(userCollectionUpdatedFields).length > 0) {
-    const updatedUser = await User.findByIdAndUpdate(id, { $set: userCollectionUpdatedFields }, { new: true, session });
-  
-    if (!updatedUser) {
-      throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to update user data');
-    }
-  }
-
-  const {budget, duration, budgetUpdate} = restData;
-
-  if (Boolean(budgetUpdate)) { 
-    if(!budget || !duration){
-      throw new ApiError(StatusCodes.BAD_REQUEST, 'Budget and duration are required');
-    }
-
-    const company = await Company.findById(isUserExist.company).session(session);
-    if (!company) {
-      throw new ApiError(StatusCodes.NOT_FOUND, 'Requested company does not exist');
-    }
-
-    const totalBudget = company.totalBudget + budget!;
-    company.totalBudget = totalBudget;
-
-
-    const updatedCompany = await company.save({ session });
-
-    if (!updatedCompany) {  
-      throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to update company data');
-    }
-
-    const startDate = new Date()
-    
-    const endDate = new Date(startDate)
-    endDate.setMonth(startDate.getMonth() + duration!)
-    
-    const endDateISO = endDate.toISOString()
-    
-    restData.budgetAssignedAt = startDate;
-    restData.budgetExpiredAt = endDateISO as unknown as Date
-    restData.budget= budget;
-    restData.totalBudget = budget! + isUserExist.budget || 0;
-    restData.budgetLeft = budget! + isUserExist.budgetLeft || 0;
-    restData.duration = duration!;
-
-
-  }
-
-  const updatedEmployee = await Employee.findOneAndUpdate({ user: id }, { $set: restData }, {
-    new: true,
-    session
-  });
-
-  if (!updatedEmployee) {
-    throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to update employee data');  
-  }
-
-  await session.commitTransaction();
-  return updatedEmployee;
-} catch (error) {
-  await session.abortTransaction();
-  throw error;
-} finally {
-  session.endSession();
-}
 
 };
 
 
-const updateCompany = async (id: Types.ObjectId, payload:Partial<ICompany & IUser>) => {
+const updateCompany = async (id: Types.ObjectId, payload: Partial<ICompany & IUser>) => {
   const isCompanyExist = await Company.findById(id);
   if (!isCompanyExist) {
     throw new ApiError(StatusCodes.NOT_FOUND, 'Requested company does not exist');
   }
-  console.log(isCompanyExist,"isCompanyExist")
+  console.log(isCompanyExist, "isCompanyExist")
 
   const updatedCompany = await User.findByIdAndUpdate(isCompanyExist.user, { $set: payload }, { new: true });
   if (!updatedCompany) {
     throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to update company data');
   }
-  console.log(updatedCompany,"updatedCompany")
+  console.log(updatedCompany, "updatedCompany")
   return updatedCompany;
 };
 
 const getAllAdmin = async () => {
-  const admin = await User.find({ role: {$in: [USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN]}  });
+  const admin = await User.find({ role: { $in: [USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN] } });
 
   return admin;
 };
